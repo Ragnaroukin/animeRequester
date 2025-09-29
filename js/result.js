e90020fc0ee1d64d73b1bccff32c6115ad957772
Martin <lemesle243@gmail.com>
2025-09-29 13:01:05 +0200

    génération des cartes, améliorer le css

--- js/result.js
+++ js/result.js
@@ -261,6 +261,40 @@ const resultExample = {
     }
   };
 
-  let tab = resultExample.data;
+  const container = document.getElementById("containerMain");
 
-  console.log(tab);
+  let data = resultExample.data;
+  let dataIds = data.map(item => item._id);
+  let titles = data.map(item => item.title);
+  let images = data.map(item => item.image);
+  let genres = data.map(item => item.genres);
+  let synopsis = data.map(item => item.synopsis);
+for (let i = 0; i < data.length; i++) {
+    if (i%3 === 0) {
+        var cardGroup = document.createElement("div");
+        cardGroup.className = "card-group";
+        container.appendChild(cardGroup);
+    }
+        if (i < data.length) {
+            var card = document.createElement("div");
+            card.className = "card";
+
+card.innerHTML = `
+    <img src="${images[i]}" class="card-img-top card-internal" alt="${titles[i]}">
+    <div class="card-body">
+        <h5 class="card-title">${titles[i]}</h5>
+        <p class="card-text">${synopsis[i].substring(0, 100)}...</p>
+        <p class="card-text"><small class="text-muted">Genres: ${genres[i].join(", ")}</small></p>
+        
+        <div class="synopsis-hover">
+            <p class="card-text"><strong>Synopsis Complet:</strong></p>
+            <p class="card-text">${synopsis[i]}</p>
+        </div>
+    </div>
+`;
+
+            cardGroup.appendChild(card);
+        }
+    }
+
+  
