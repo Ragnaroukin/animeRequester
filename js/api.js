9d3da6399aeeb5047a6fa1b45749b341f5c16572
picot244 <solal.picot@etu.unicaen.fr>
2025-09-30 12:26:13 +0200

    [FIX] correction de la génération de l'url

--- js/api.js
+++ js/api.js
@@ -1,38 +1,44 @@
 const data = null;
-const result = document.getElementById("result");
 const xhr = new XMLHttpRequest();
+const form = document.getElementById('form');
+let url;
 
-const promise = new Promise((resolve, reject) => {
-    let url;
-    xhr.withCredentials = true;
-    if(__POST['nom'] !== 'undefined')
-        url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10' + __POST['nom'];
-    if(__POST['classement'] !== 'undefined')
-        url = 'https://anime-db.p.rapidapi.com/anime/by-ranking/' + __POST['classement'];
-    if(__POST['id'] !== 'undefined')
-        url = 'https://anime-db.p.rapidapi.com/anime/by-id/' + __POST['id'];
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const fd = new FormData(form);
+    nom = fd.get('nom');
+    classement = fd.get('classement');
+    id = fd.get('id');
+    if (nom) url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=' + nom;
+    if (classement) url = 'https://anime-db.p.rapidapi.com/anime/by-ranking/' + classement;
+    if (id) url = 'https://anime-db.p.rapidapi.com/anime/by-id/' + id;
 
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                resolve(xhr.responseText); 
-            } else {
-                reject(xhr.status); 
-            }
-        }
-    };
-
-    xhr.open('GET',url);
+    xhr.open('GET', url);
     xhr.setRequestHeader('x-rapidapi-key', '83e099eae4msh95fcf898d140624p1dcfdejsn9577d139c852');
     xhr.setRequestHeader('x-rapidapi-host', 'anime-db.p.rapidapi.com');
 
     xhr.send(data);
 });
 
+const promise = new Promise((resolve, reject) => {
+    xhr.withCredentials = true;
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.responseText);
+            } else {
+                reject(xhr.status);
+            }
+        }
+    };
+});
+
 promise
     .then(res => {
-       result.innerHTML = res;
+        console.log(res);
     })
     .catch(err => {
+        console.log(url);
         console.error("Erreur:", err);
     });
