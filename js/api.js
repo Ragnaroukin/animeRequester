f980121f8bcc5a0e69ec7ae98cce8c5958479437
picot244 <solal.picot@etu.unicaen.fr>
2025-09-30 11:12:47 +0200

    [FEATURE] changement de l'url en fonction de la recherche

--- js/api.js
+++ js/api.js
@@ -3,7 +3,14 @@ const result = document.getElementById("result");
 const xhr = new XMLHttpRequest();
 
 const promise = new Promise((resolve, reject) => {
+    let url;
     xhr.withCredentials = true;
+    if(__POST['nom'] !== 'undefined')
+        url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10' + __POST['nom'];
+    if(__POST['classement'] !== 'undefined')
+        url = 'https://anime-db.p.rapidapi.com/anime/by-ranking/' + __POST['classement'];
+    if(__POST['id'] !== 'undefined')
+        url = 'https://anime-db.p.rapidapi.com/anime/by-id/' + __POST['id'];
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
@@ -15,10 +22,7 @@ const promise = new Promise((resolve, reject) => {
         }
     };
 
-    xhr.open(
-        'GET',
-        'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc'
-    );
+    xhr.open('GET',url);
     xhr.setRequestHeader('x-rapidapi-key', '83e099eae4msh95fcf898d140624p1dcfdejsn9577d139c852');
     xhr.setRequestHeader('x-rapidapi-host', 'anime-db.p.rapidapi.com');
 
