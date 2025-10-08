a556fe667f7100c6d0af632aa83ffec1a5c2e79c
picot244 <solal.picot@etu.unicaen.fr>
2025-10-08 15:43:26 +0200

    [FEATURE] import/export du resultat

--- js/api.js
+++ js/api.js
@@ -1,7 +1,9 @@
+export let result;
 const data = null;
 const xhr = new XMLHttpRequest();
 const form = document.getElementById('form');
 let url;
+let isOneLine;
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -9,10 +11,18 @@ form.addEventListener('submit', e => {
     nom = fd.get('nom');
     classement = fd.get('classement');
     id = fd.get('id');
-    if (nom) url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=' + nom;
-    if (classement) url = 'https://anime-db.p.rapidapi.com/anime/by-ranking/' + classement;
-    if (id) url = 'https://anime-db.p.rapidapi.com/anime/by-id/' + id;
-
+    if (nom) {
+        url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=' + nom;
+        isOneLine = false;
+    }
+    if (classement) {
+        url = 'https://anime-db.p.rapidapi.com/anime/by-ranking/' + classement;
+        isOneLine = true;
+    }
+    if (id) {
+        url = 'https://anime-db.p.rapidapi.com/anime/by-id/' + id;
+        isOneLine = true;
+    }
     xhr.open('GET', url);
     xhr.setRequestHeader('x-rapidapi-key', '83e099eae4msh95fcf898d140624p1dcfdejsn9577d139c852');
     xhr.setRequestHeader('x-rapidapi-host', 'anime-db.p.rapidapi.com');
@@ -36,7 +46,10 @@ const promise = new Promise((resolve, reject) => {
 
 promise
     .then(res => {
-        console.log(res);
+        if(isOneLine)
+            result = {res};
+        else
+            result = res;
     })
     .catch(err => {
         console.log(url);
