d139722c08b73c683b8f8e79c12d93c97d1700cc
Arthur POIRIER--DUBOIS <arthurpoidub@gmail.com>
2025-10-09 15:23:20 +0200

    fusion form

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-const hr = new XMLHttpRequest();
-hr.withCredentials = true;
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
 
-hr.addEventListener('readystatechange', function () {
+xhr.addEventListener('readystatechange', function () {
+
     if (this.readyState === this.DONE) {
         // Parse la réponse JSON
         const data = JSON.parse(this.responseText);
@@ -11,12 +12,13 @@ hr.addEventListener('readystatechange', function () {
     }
 });
 
-hr.open('GET', 'https://anime-db.p.rapidapi.com/genre');
-hr.setRequestHeader('x-rapidapi-key', '589ffe3c27mshed298ed00c88ee4p150c58jsnec449e25eca6');
-hr.setRequestHeader('x-rapidapi-host', 'anime-db.p.rapidapi.com');
-hr.send();
+xhr.open('GET', 'https://anime-db.p.rapidapi.com/genre');
+xhr.setRequestHeader('x-rapidapi-key', '1be4fdd298msh8dc37149279e2acp1ec290jsn6626a55c72d2');
+xhr.setRequestHeader('x-rapidapi-host', 'anime-db.p.rapidapi.com');
+xhr.send();
 
 const container = document.getElementById("genre");
+let sombreButton = document.getElementById("darkmode");
 
 function create_genre(data) {
     let dataGenres = data.map(item => item._id); // maintenant ça marche
@@ -36,3 +38,23 @@ function create_genre(data) {
         group.appendChild(checkbox);
     }
 }
+
+sombreButton.addEventListener("click", function() {
+    
+      if (sombreButton.classList.contains("btn-dark")) {
+        sombreButton.textContent = "Mode clair";
+        sombreButton.classList.remove("btn-dark");
+        sombreButton.classList.add("btn-light");
+        document.documentElement.style.setProperty('--couleur1', '#f8f9fa');
+        document.documentElement.style.setProperty('--couleur2', 'rgb(66, 66, 66)');
+      } else {
+        sombreButton.textContent = "Mode sombre";
+        sombreButton.classList.remove("btn-light");
+        sombreButton.classList.add("btn-dark");
+        document.documentElement.style.setProperty('--couleur1', 'rgb(66, 66, 66)'); // fond principal
+        document.documentElement.style.setProperty('--couleur2', '#f8f9fa'); // éléments secondaires/transparents
+
+
+
+      }
+});
