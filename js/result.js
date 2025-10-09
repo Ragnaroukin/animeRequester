905b1c6d7c0fc8b96e332dc9256b8a9882f23ae0
Arthur POIRIER--DUBOIS <arthurpoidub@gmail.com>
2025-10-09 15:27:39 +0200

    fusion result

--- js/result.js
+++ js/result.js
@@ -282,7 +282,6 @@ for (let i = 0; i < data.length; i++) {
   if (i < data.length) {
     var card = document.createElement("div");
     card.className = "card";
-
     card.innerHTML = `
     <img src="${images[i]}" class="card-img-top card-internal" alt="${titles[i]}">
     <div class="card-body">
@@ -290,16 +289,41 @@ for (let i = 0; i < data.length; i++) {
         <div class="shortsynopsis">
             <p class="card-text">${synopsis[i].substring(0, 100)}...</p>
         </div>
-        <p class="card-text"><small class="text-muted">Genres: ${genres[i].join(", ")}</small></p>
-        
+        <p class="card-text"><small class="genre">Genres: ${genres[i].join(", ")}</small></p>
         <div class="synopsis-hover">
             <p class="card-text"><strong>Synopsis Complet:</strong></p>
             <p class="card-text">${synopsis[i]}</p>
         </div>
-    </div>
-`;
-
+    </div>`;
     cardGroup.appendChild(card);
   }
 }
+
+sombreButton.addEventListener("click", toggleSombreMode);
+
+
+function toggleSombreMode() {
+  if (sombreButton.classList.contains("btn-dark")) {
+    sombreButton.textContent = "Mode clair";
+    sombreButton.classList.remove("btn-dark");
+    sombreButton.classList.add("btn-light");
+    document.documentElement.style.setProperty('--couleur1', '#f8f9fa');
+    document.documentElement.style.setProperty('--couleur2', 'rgba(0.5, 0.5, 0.5, 0.5)');
+    document.documentElement.style.setProperty('--couleur3', '#ffffff');
+    document.documentElement.style.setProperty('--couleur4', 'rgb(233, 233, 233)');
+    document.documentElement.style.setProperty('--couleur5', '#f8f9fa');
+    document.documentElement.style.setProperty('--couleur6', 'black');
+  } else {
+    sombreButton.textContent = "Mode sombre";
+    sombreButton.classList.remove("btn-light");
+    sombreButton.classList.add("btn-dark");
+    document.documentElement.style.setProperty('--couleur1', '#121212');
+    document.documentElement.style.setProperty('--couleur2', 'rgba(255, 255, 255, 0.1)');
+    document.documentElement.style.setProperty('--couleur3', '#1e1e1e');
+    document.documentElement.style.setProperty('--couleur4', '#2c2c2c');
+    document.documentElement.style.setProperty('--couleur5', '#0d0d0d'); 
+    document.documentElement.style.setProperty('--couleur6', 'white'); 
+  }
+}
+
 
